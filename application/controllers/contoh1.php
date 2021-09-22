be6e17ef10a06ec4b6ec01dd81f4d79359ab50d5
harniaff <harniafebrian17@gmail.com>
2021-09-22 10:38:55 +0700

    upload awal project

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan Diri</h1>";
+        echo"<p>Nama saya adalah Harnia Febrian Fathoni</p>
+            <p>Nama panggilannya Harnia</p>
+            <p>Nim 12200616</p>
+            <p>Alasan saya berkuliah di UBSI karena biayanya yang terjangkau dan juga jaraknya dekat dari rumah</p>
+            <p>Hobby saya adalah menulis dan mendengarkan musik</p>";
+    }
+}
